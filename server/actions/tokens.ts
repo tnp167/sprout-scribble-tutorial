7f2b963d96001d71cf530cfa45306da774f69770
tnp_167 <tnp_167@outlook.com>
2024-08-17 14:51:58 +0100

    Add email verification feature

--- server/actions/tokens.ts
+++ server/actions/tokens.ts
@@ -1,7 +1,7 @@
 "use server";
 
 import { eq } from "drizzle-orm";
-import { emailTokens } from "../schema";
+import { emailTokens, users } from "../schema";
 import { db } from "..";
 
 export const getVerificationTokenByEmail = async (email: string) => {
@@ -35,4 +35,28 @@ export const generateEmailVerificationToken = async (email: string) => {
     .returning();
 
   return verificationToken;
+};
+
+export const newVerification = async (token: string) => {
+  const existingToken = await getVerificationTokenByEmail(token);
+  if (!existingToken) return { error: "Token not found" };
+
+  const hasExpired = new Date(existingToken.expires) < new Date();
+  if (hasExpired) return { error: "Token has expired" };
+
+  const existingUser = await db.query.users.findFirst({
+    where: eq(users.email, existingToken.email),
+  });
+  if (!existingUser) return { error: "Email does not exist" };
+
+  await db
+    .update(users)
+    .set({
+      emailVerified: new Date(),
+      email: existingToken.email,
+    })
+    .where(eq(users.id, existingUser.id));
+
+  await db.delete(emailTokens).where(eq(emailTokens.id, existingToken.id));
+  return { success: "Email Verified" };
 };
